455f9d4efde6b0215c82ee4c8a2bebe64c4040db
Aniraj1 <anirajshahi@gmail.com>
2022-07-17 23:07:10 +0545

    db

--- Crime Reporting System User Interface/Database/database.php
+++ Crime Reporting System User Interface/Database/database.php
@@ -10,7 +10,7 @@ $sql_admin = "CREATE TABLE Admin (
     address VARCHAR(40) NOT NULL,
     contact_no CHAR(10) Unique,
     land_line_no CHAR(10) Unique,
-    email_id VARCHAR(40) NOT NULL,
+    email_id VARCHAR(40) NOT NULL
     )";
 
 
@@ -19,6 +19,25 @@ $sql_admin = "CREATE TABLE Admin (
     } else {
         echo "Error creating table: " . $conn->error;
     }
+
+    $sql_user = "CREATE TABLE User (
+        user_id int(10) AUTO_INCREMENT PRIMARY KEY,
+        f_name VARCHAR(30) NOT NULL,
+        l_name VARCHAR(30) NOT NULL,
+        username VARCHAR(50) NOT NULL UNIQUE,
+        password VARCHAR(40) NOT NULL,
+        liscence_no VARCHAR(40) NOT NULL,
+        city VARCHAR(40),
+        address VARCHAR(40) NOT NULL,
+        near_police_station VARCHAR(20)
+        )";
+    
+    
+        if ($conn->query($sql_admin) === TRUE) {
+            echo "2.  User table created successfully <br/>";
+        } else {
+            echo "Error creating table: " . $conn->error;
+        }
 
 
     $sql_police_station = "CREATE TABLE Police_Station (
@@ -32,7 +51,7 @@ $sql_admin = "CREATE TABLE Admin (
     
     
         if ($conn->query($sql_police_station) === TRUE) {
-            echo "2.  Police station table created successfully <br/>";
+            echo "3.  Police station table created successfully <br/>";
         } else {
             echo "Error creating table: " . $conn->error;
         }
@@ -52,14 +71,14 @@ $sql_admin = "CREATE TABLE Admin (
         )";
 
     if ($conn->query($sql_police_reg) === TRUE) {
-        echo "3. police_reg table created successfully <br/>";
+        echo "4. police_reg table created successfully <br/>";
     } else {
         echo "Error creating table: " . $conn->error;
     }
 
     
     $sql_criminal_details = "CREATE TABLE Criminal_Details (
-        criminal_name PRIMARY KEY,
+        criminal_name varchar(50) PRIMARY KEY,
         age int(5) NOT NULL,  
         height FLOAT(10) NOT NULL,
         weight FLOAT(10) NOT NULL,
@@ -72,63 +91,140 @@ $sql_admin = "CREATE TABLE Admin (
         image longblob NOT NULL
         )";
 
-    if ($conn->query($sql_police_reg) === TRUE) {
-        echo "4. criminal_details table created successfully <br/>";
+    if ($conn->query($sql_criminal_details) === TRUE) {
+        echo "5. criminal_details table created successfully <br/>";
     } else {
         echo "Error creating table: " . $conn->error;
     }
 
 
-
-    $sql_company = "CREATE TABLE Company (
-        company_id int(10) AUTO_INCREMENT  PRIMARY KEY,
-        company_username VARCHAR(30) NOT NULL,
-        company_name VARCHAR(50) NOT NULL,
-        Email VARCHAR(40) NOT NULL Unique,
-        Password CHAR(16),
-        contact_num CHAR(10) Unique,
-        agent_id int(10),
-        FOREIGN KEY(agent_id) REFERENCES Agent(agent_id)
+    $sql_crimes = "CREATE TABLE Crimes (
+        crime_id  AUTO_INCREMENT PRIMARY KEY,
+        complaint_id int(10) FOREIGN KEY REFERENCES User_Complaint(complaint_id),
+        user_id int(10) FOREIGN KEY REFERENCES User(user_id),
+        admin_id int(10) FOREIGN KEY REFERENCES User(admin_id),
+        crime_place varchar(50) NOT NULL,  
+        crime_type varchar(50) NOT NULL,
+        crime_evidence varchar(50) NOT NULL,
+        image longblob NOT NULL,
+        missing_id int(10) FOREIGN KEY REFERENCES Missing_Details(missing_id),
+        sms_mobile_no int(10) FOREIGN KEY REFERENCES SMS(sms_mobile_no),
+        criminal_name varchar(50) FOREIGN KEY REFERENCES Criminal_Details(criminal_name)
         )";
 
-    if ($conn->query($sql_company) === TRUE) {
-        echo "3. Company table created successfully <br/>";
+
+    if ($conn->query($sql_crimes) === TRUE) {
+        echo "6. Crimes table created successfully <br/>";
     } else {
         echo "Error creating table: " . $conn->error;
     }
 
-    $sql_task = "CREATE TABLE Task (
-        task_id int(20) AUTO_INCREMENT PRIMARY KEY,
-        company_id int(10),
-        subject VARCHAR(50) NOT NULL,
-        task VARCHAR(40) NOT NULL,
-        report VARCHAR (90),
-        status tinyint(1) default 0,
-        due_date date,
-        closed_date date,
-        FOREIGN KEY(company_id) REFERENCES Company(company_id)
+
+    $sql_investigation = "CREATE TABLE Investigation (
+        investigation_id int(10) AUTO_INCREMENT  PRIMARY KEY,
+        crime_id int(10) FOREIGN KEY REFERENCES Crimes(crime_id),
+        complaint_id int(10) FOREIGN KEY REFERENCES User_Complaint(complaint_id),
+        criminal_name varchar(50) FOREIGN KEY REFERENCES Criminal_Details(criminal_name),
+        station_id int(10) FOREIGN KEY REFERENCES Police_Station(station_id),
+        investigation_del_id int(10) FOREIGN KEY REFERENCES Investigation_Details(investigation_del_id)
+
         )";
 
-    if ($conn->query($sql_task) === TRUE) {
-        echo "4. Task table created successfully <br/>";
+    if ($conn->query($sql_investigation) === TRUE) {
+        echo "7. Investigation table created successfully <br/>";
     } else {
         echo "Error creating table: " . $conn->error;
     }
-    $sql_contact = "CREATE TABLE Contactus (
-        contactus_id int(20) AUTO_INCREMENT PRIMARY Key,
-        F_name VARCHAR(50) NOT NULL,
-        company_name VARCHAR(50) NOT NULL,
-        Email VARCHAR(40) NOT NULL,
-        contact_num CHAR(10) NOT NULL,
-        subject VARCHAR(30) NOT NULL,
-        message VARCHAR(180) NOT NULL,
-        mgs_date DATE
+
+
+    $sql_investigation_del = "CREATE TABLE Investigation_Details (
+        investigation_del_id int(10) AUTO_INCREMENT  PRIMARY KEY,
+        details VARCHAR(500),
+        image longblob NOT NULL,
+        evidences VARCHAR(500) NOT NULL,
+        police_id int(10) FOREIGN KEY REFERENCES Police_Registration(police_id)
         )";
 
-    if ($conn->query($sql_contact) === TRUE) {
-        echo "5. Contact table created successfully <br/>";
+    if ($conn->query($sql_investigation_del) === TRUE) {
+        echo "8. Investigation_Details table created successfully <br/>";
     } else {
         echo "Error creating table: " . $conn->error;
     }
+
+
+    $sql_user_complaints = "CREATE TABLE User_Complaints (
+        complaint_id int(10) AUTO_INCREMENT  PRIMARY KEY,
+        user_id int(10) FOREIGN KEY REFERENCES User(user_id),
+        crime_place varchar(50) NOT NULL,  
+        crime_type varchar(50) NOT NULL,
+        crime_evidence varchar(50) NOT NULL,
+        image longblob NOT NULL
+        )";
+
+    if ($conn->query($sql_user_complaints) === TRUE) {
+        echo "9. User Complaints table created successfully <br/>";
+    } else {
+        echo "Error creating table: " . $conn->error;
+    }
+
+
+
+    $sql_sms = "CREATE TABLE SMS (
+        sms_mobile_no int(10) AUTO_INCREMENT  PRIMARY KEY,
+        complaint varchar(500) not null
+        )";
+
+    if ($conn->query($sql_sms) === TRUE) {
+        echo "10. SMS table created successfully <br/>";
+    } else {
+        echo "Error creating table: " . $conn->error;
+    }
+
+
+
+    $sql_search= "CREATE TABLE Search (
+        
+        criminal_name varchar(50) FOREIGN KEY REFERENCES Criminal_Details(criminal_name),
+        station_id int(10) FOREIGN KEY REFERENCES Police_Station(station_id),
+        admin_id int(10) FOREIGN KEY REFERENCES User(admin_id),
+        user_id int(10) FOREIGN KEY REFERENCES User(user_id),
+        police_id int(10) FOREIGN KEY REFERENCES Police_Registration(police_id)
+                
+        )";
+
+    if ($conn->query($sql_search) === TRUE) {
+        echo "11. search table created successfully <br/>";
+    } else {
+        echo "Error creating table: " . $conn->error;
+    }
+
+
+
+    $sql_missing_details = "CREATE TABLE Missing_Details (
+        missing_id int(10) AUTO_INCREMENT  PRIMARY KEY,
+        name varchar(50) NOT NULL,
+        address varchar(50) NOT NULL, 
+        age int(5) NOT NULL,  
+        height FLOAT(10) NOT NULL,
+        weight FLOAT(10) NOT NULL,
+        gender varchar(6) Not NULL,
+        hair_type varchar(20) NOT NULL,
+        skin_color Varchar(20) NOT NULL, 
+        race varchar(10),
+        description varchar(700) NOT NULL, 
+        missing_date DATE,
+        image longblob NOT NULL
+        )";
+
+    if ($conn->query($sql_missing_details) === TRUE) {
+        echo "12. missing_details table created successfully <br/>";
+    } else {
+        echo "Error creating table: " . $conn->error;
+    }
+
+
+
+
+
     mysqli_close($conn);
      ?>
