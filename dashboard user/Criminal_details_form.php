e25072b43f974351956d72fe345246c452881bca
Forlorn4eva <darko123Z>
2022-07-26 09:11:22 +0545

    quality of life changes

--- dashboard user/Criminal_details_form.php
+++ dashboard user/Criminal_details_form.php
@@ -173,7 +173,7 @@ include "connection.php";
                             <span class="d-none d-lg-inline-flex">John Doe</span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end bg-secondary border-0 rounded-0 rounded-bottom m-0">
-                            <a href="#" class="dropdown-item">My Profile</a>
+                            <a href="Profile.php" class="dropdown-item">My Profile</a>
                             <a href="#" class="dropdown-item">Settings</a>
                             <a href="#" class="dropdown-item">Log Out</a>
                         </div>
