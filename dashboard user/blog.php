acf5b14457071deb78b4eea127fa192bb09587ab
XshrineX <nischalthapa10@hotmail.com>
2022-07-28 13:17:52 +0545

    gg

--- dashboard user/blog.php
+++ dashboard user/blog.php
@@ -81,7 +81,7 @@
                         </div>
                     </div>
                    <!--<a href="widget.php" class="nav-item nav-link"><i class="fa fa-th me-2"></i>Widgets</a>-->
-                    <a href="blog.php" class="nav-item nav-link active"><i class="fa fa-chart-bar me-2"></i>Blog/News</a>
+                    <!-- <a href="blog.php" class="nav-item nav-link active"><i class="fa fa-chart-bar me-2"></i>Blog/News</a> -->
 
                     <a href="Table_criminal.php" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>Criminal detail</a>
                     
