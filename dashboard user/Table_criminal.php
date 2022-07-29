7dd7dda10e9dfb8d41e7d374c58145f8791ec08f
Forlorn4eva <darko123Z>
2022-07-29 23:26:42 +0545

    gg

--- dashboard user/Table_criminal.php
+++ dashboard user/Table_criminal.php
@@ -35,7 +35,7 @@ $image="";
 $no=0;
  
 include ("database_handler/DB_pull.php");
-
+include("database_handler/select_user.php");
    
    ?>
 <!DOCTYPE html>
@@ -90,7 +90,7 @@ include ("database_handler/DB_pull.php");
                 </a>
                 <div class="d-flex align-items-center ms-4 mb-4">
                     <div class="position-relative">
-                        <img class="rounded-circle" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                        <img class="rounded-circle" src="http://localhost/crime_reporting/dashboard%20user/image/<?php echo $image1;?>" alt="" style="width: 40px; height: 40px;">
                         <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
                     </div>
                     <div class="ms-3">
@@ -206,7 +206,7 @@ include ("database_handler/DB_pull.php");
                     </div>
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                            <img class="rounded-circle me-lg-2" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
+                            <img class="rounded-circle me-lg-2" src="http://localhost/crime_reporting/dashboard%20user/image/<?php echo $image1;?>" alt="" style="width: 40px; height: 40px;">
                             <span class="d-none d-lg-inline-flex">John Doe</span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end bg-secondary border-0 rounded-0 rounded-bottom m-0">
